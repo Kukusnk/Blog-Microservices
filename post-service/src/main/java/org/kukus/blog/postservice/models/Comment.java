64d9b9f12ae87c2e7f831a188a4207a53724ac8c
nik <sony.epam@gmail.com>
2025-06-03 15:31:50 +0300

    Deleted EnrichedPostDTO.java, the enrichment logic has moved to another service

--- post-service/src/main/java/org/kukus/blog/postservice/models/Comment.java
+++ post-service/src/main/java/org/kukus/blog/postservice/models/Comment.java
@@ -1,10 +1,12 @@
 package org.kukus.blog.postservice.models;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Data
 public class Comment {
     @Id
     private String id = UUID.randomUUID().toString(); // Generate UUID for id
@@ -22,37 +24,5 @@ public class Comment {
         this.content = content;
         this.authorId = authorId;
         this.createdAt = LocalDateTime.now();
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getAuthorId() {
-        return authorId;
-    }
-
-    public void setAuthorId(String authorId) {
-        this.authorId = authorId;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
     }
 }
